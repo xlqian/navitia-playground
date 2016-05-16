75e718da18215d8fc2f0814e2ee40a5460f18bd5
Guillaume Pinot <texitoi@texitoi.eu>
2016-05-16 17:21:00 +0200

    first summary view with toogle to code

--- js/response.js
+++ js/response.js
@@ -2,6 +2,81 @@ function setStatus(xhr) {
     $("#status").html(xhr.statusText + " (" + xhr.status + ")");
 }
 
+function defaultSummary(json) {
+    var result = $('<span/>');
+    if ('label' in json) {
+        result.html(json['label']);
+    } else if ('code' in json) {
+        result.html(json['code']);
+    }else if ('name' in json) {
+        result.html(json['name']);
+    } else if ('id' in json) {
+        result.html(json['id']);
+    }
+    if ('color' in json) {
+        result.css('background-color', '#' + json.color);
+    }
+    if ('text_color' in json) {
+        result.css('color', '#' + json['text_color']);
+    }
+    return result;
+}
+
+function responseSummary(json) {
+    var key = ''
+    var objs = [];
+    for (var k in json) {
+        if ($.isArray(json[k])
+            && k != 'disruptions'
+            && k != 'links'
+            && k != 'feed_publishers'
+            && k != 'tickets'
+            && k != 'exceptions'
+            && k != 'notes') {
+            objs = json[k];
+            key = k;
+        }
+    }
+    // disruptions may be an object list only if there is no other object list
+    if (objs.length == 0 && 'disruptions' in json) {
+        objs = json['disruptions'];
+        key = 'disruptions';
+    }
+    // remove plural of collection type
+    if (key.slice(-1) == 's') {
+        key = key.slice(0, -1);
+    }
+    var result = $('<ul/>');
+    objs.forEach(function(obj) {
+        $('<li/>').html(render(key, obj)).appendTo(result);
+    });
+    return result;
+}
+
+function summary(name, json) {
+    if (name == 'response') {
+        return responseSummary(json);
+    }
+    // add here custom summary
+    return defaultSummary(json);
+}
+
+function render(name, json) {
+    var head = $('<div class="head"></span>');
+    head.append($('<span class="name">{0}</span>'.format(name)));
+    head.append($('<button class="code">{}</button>').click(function() {
+        $(this).parent().parent().children(".data").html(renderjson(json));
+    }));
+    head.append($('<button class="summary">...</button>').click(function() {
+        $(this).parent().parent().children(".data").html(summary(name, json));
+    }));
+
+    var result = $('<div class="render"></div>');
+    result.append(head);
+    result.append($('<span class="data"></span>').html(summary(name, json)));
+    return result
+}
+
 $(document).ready(function() {
     var search = new URI(window.location).search(true);
     var request = search["request"];
@@ -10,6 +85,7 @@ $(document).ready(function() {
         $("#data").html("No request");
         return;
     }
+    renderjson.set_show_to_level(1);
     $.ajax({
         headers: isUndefined(token) ? {} : { Authorization: "Basic " + btoa(token + ":" ) },
         url: request,
@@ -17,7 +93,7 @@ $(document).ready(function() {
     }).then(
         function(data, status, xhr) {
             setStatus(xhr);
-            $("#data").html(renderjson(data));
+            $("#data").html(render("response", data));
         },
         function(xhr, status, error) {
             setStatus(xhr);
