5ceb6b2f86fd720a7560d7ec941b0085987045b1
xlqian <patrick.qian@canaltp.fr>
2016-05-17 09:38:21 +0200

    update config

--- gulpfile.js
+++ gulpfile.js
@@ -20,134 +20,133 @@ var config = require('./config');
 var delConfig = config.del;
 
 function isProd(env) {
-  return env == 'prod';
+    return env == 'prod';
 }
 // Lint Task
 gulp.task('lint', function() {
-  return gulp.src('js/*.js')
-      .pipe(jshint())
-      .pipe(jshint.reporter('default'));
+    return gulp.src('js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
 
 // Compile Sass
 function compile_sass(env) {
-  return function() {
-    return gulp.src('scss/**/*.scss')
-      .pipe(sass())
-      .pipe(gulpFilter('**/style.css'))
-      .pipe(gulpif(isProd(env), cleanCSS()))
-      .pipe(gulp.dest(config[env] + '/css'));
-  }
+    return function() {
+        return gulp.src('scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulpFilter('**/style.css'))
+        .pipe(gulpif(isProd(env), cleanCSS()))
+        .pipe(gulp.dest(config[env] + '/css'));
+    }
 }
 gulp.task('dev:sass', compile_sass('dev'));
 gulp.task('prod:sass', compile_sass('prod'));
 
 // Concatenate & Minify JS
 function compile_js(env) {
-  return function () {
-    return gulp.src('js/**/*.js')
+    return function () {
+        return gulp.src('js/**/*.js')
         .pipe(concat('app.js'))
         .pipe(gulpif(isProd(env), uglify()))
         .pipe(rename('app.min.js'))
         .pipe(gulp.dest(config[env] + '/js'));
-  }
+    }
 }
 gulp.task('dev:scripts', compile_js('dev'));
 gulp.task('prod:scripts', compile_js('prod'));
 
 // Concatenate & Minify vendor lib
 function compile_vendor(env){
-  return function(){
-    return bower()
-      .pipe(gulpFilter([
-        '**/dist/jquery.js',
-        '**/renderjson.js',
-        '**/urijs/src/URI.js',
-        '!**/*.min.js']))
-      .pipe(concat('lib.js'))
-      .pipe(gulpif(isProd(env),uglify()))
-      .pipe(rename('lib.min.js'))
-      .pipe(gulp.dest(config[env] + '/lib'));
-  }
+    return function(){
+        return bower()
+        .pipe(gulpFilter([
+            '**/dist/jquery.js',
+            '**/renderjson.js',
+            '**/urijs/src/URI.js',
+            '!**/*.min.js']))
+            .pipe(concat('lib.js'))
+            .pipe(gulpif(isProd(env),uglify()))
+            .pipe(rename('lib.min.js'))
+            .pipe(gulp.dest(config[env] + '/lib'));
+    }
 }
 gulp.task('dev:bower', compile_vendor('dev'));
 gulp.task('prod:bower', compile_vendor('prod'));
 
 // Compress img
 function compress_img(env) {
-  return function(){
-    return gulp.src('img/**')
-      .pipe(image({
-        pngquant: true,
-        optipng: false,
-        zopflipng: true,
-        advpng: true,
-        jpegRecompress: false,
-        jpegoptim: true,
-        mozjpeg: true,
-        gifsicle: true,
-        svgo: true
-      }))
-      .pipe(gulp.dest(config[env] + '/img'));
-  }
+    return function(){
+        return gulp.src('img/**')
+            .pipe(image({
+                pngquant: true,
+                optipng: false,
+                zopflipng: true,
+                advpng: true,
+                jpegRecompress: false,
+                jpegoptim: true,
+                mozjpeg: true,
+                gifsicle: true,
+                svgo: true
+            }))
+            .pipe(gulp.dest(config[env] + '/img'));
+    }
 }
 gulp.task('dev:img', compress_img('dev'));
 gulp.task('prod:img', compress_img('prod'));
 
 // Minify html
 function compile_html(env){
-  return function() {
-    return gulp.src('app/*.html')
-      .pipe(htmlmin({collapseWhitespace: isProd(env)}))
-      .pipe(gulp.dest(config[env]))
-  }
+    return function() {
+        return gulp.src('app/*.html')
+        .pipe(htmlmin({collapseWhitespace: isProd(env)}))
+        .pipe(gulp.dest(config[env]))
+    }
 }
 gulp.task('dev:minify_html', compile_html('dev'));
 gulp.task('prod:minify_html', compile_html('prod'));
 
 // Watch Files For Changes
-gulp.task('watch', function() {
-  gulp.watch('js/**/*.js', ['dev:scripts']);
-  gulp.watch('scss/**/*.scss', ['dev:sass']);
-  gulp.watch('img/**', ['dev:img']);
-  gulp.watch('app/**', ['dev:minify_html']);
-  browserSync.init(config.browsersync.dev);
-});
+function watch(env){
+    return function() {
+        gulp.watch('js/**/*.js', [env + ':scripts']);
+        gulp.watch('scss/**/*.scss', [env + ':sass']);
+        gulp.watch('img/**', [env + ':img']);
+        gulp.watch('app/**', [env + ':minify_html']);
+        browserSync.init(config.browsersync[env]);
+    }
+}
+gulp.task('dev:watch', watch('dev'));
+gulp.task('prod:watch', watch('prod'));
 
+// build sequence
 function build(env) {
-  return function(cb){
-      runSequence([
+    return function(cb){
+        runSequence([
         env + ':scripts',
         env + ':sass',
         env + ':bower',
         env + ':img',
         env + ':minify_html'], cb);
-  }
+    }
 }
-
 gulp.task('dev:build', build('dev'));
-
-gulp.task('dev', function (cb) {
-    runSequence('dev:build', 'watch', cb);
-});
-
 gulp.task('prod:build', build('prod'));
 
+gulp.task('dev', function (cb) {
+    runSequence('dev:build', 'dev:watch', cb);
+});
 gulp.task('prod', function (cb) {
-    runSequence('prod:build', 'watch', cb);
+    runSequence('prod:build', 'prod:watch', cb);
 });
 
-
 gulp.task('all:clean', function(cb){
-  del(delConfig.all, cb);
+    del(delConfig.all, cb);
 })
 
 gulp.task('dev:clean', function(cb){
-  del(delConfig.dev, cb);
+    del(delConfig.dev, cb);
 })
 
 gulp.task('prod:clean', function(cb){
-  del(delConfig.prod, cb);
+    del(delConfig.prod, cb);
 })
-
-// TODO: prod
